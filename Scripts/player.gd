ea0fca9d88dc8733160965a97aaa992ea5c9b606
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-18 11:06:58 -0300

    Adding custon keys input and creating player script for moviment

---
+++ Scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed := 400
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+		
+	move_and_collide(velocity * delta)
