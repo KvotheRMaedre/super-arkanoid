d41cc0c0bfbe98a65da6606d7c5d17524edf2823
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-25 15:13:14 -0300

    Adding visual feedback when hitting a block

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -2,9 +2,15 @@ extends StaticBody2D
 
 @export var lives_block = 1
 
+@onready var sprite_node = get_node("Sprite")
+
 func take_damage() -> bool:
 	lives_block -= 1
 	if lives_block == 0:
 		queue_free()
 		return true
+	on_damage_receive()
 	return false
+
+func on_damage_receive():
+	sprite_node.modulate = Color(0.75, 0.75, 0.75, 1)
